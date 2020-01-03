99bab7ac2b6e653284ec22bf88c1791842134e34
ddelfaus <ddelfaus@gmail.com>
2020-01-03 18:20:41 -0500

    seeded data

---
+++ database/seeds/02recipe.js
@@ -0,0 +1,12 @@
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('recipe').truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex('recipe').insert([
+        {user_id: 1, title: 'test', ingredients: 'butter, salt, pepper', instructions:' do this and this and this and this'}
+
+        
+      ]);
+    });
+};
