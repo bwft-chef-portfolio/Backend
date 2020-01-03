7a49bae9720d66a1cfe6794a53f3ce64b75ecf3c
ddelfaus <ddelfaus@gmail.com>
2020-01-03 18:02:18 -0500

    made a migration

---
+++ database/migrations/20200103173034_userChefpost.js
@@ -0,0 +1,73 @@
+
+exports.up = function(knex) {
+  
+    return knex.schema
+    
+    .createTable('users', users => {
+        users.increments();
+    
+        users
+          .string('username', 255)
+          .notNullable()
+          .unique();
+        users
+        .string('password', 255)
+        .notNullable();
+
+        users
+        .string('email', 255)
+        .notNullable();
+
+        users
+        .string('first_name', 255)
+
+        users
+        .string('last_name', 255)
+
+        users
+        .string('phone', 255)
+
+        users
+        .string('location', 255)
+      })
+
+
+      .createTable('recipe', recipe => {
+        recipe.increments();
+
+        recipe
+        .integer('user_id')
+        .unsigend()
+        .notNullable()
+        .references('id')
+        .inTable('users')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+
+
+        recipe
+        .string('img_url', 350)
+
+        recipe
+        .string('title', 255)
+        .notNullable()
+
+        recipe
+        .string('description', 500)
+
+        recipe
+        .string('ingredients', 500)
+        .notNullable()
+
+        recipe
+        .string('instructions')
+        .notNullable()
+
+      })
+
+
+};
+
+exports.down = function(knex) {
+  
+};
