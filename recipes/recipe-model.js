d4037a799b2acbadf232486ab4216ef6dd8f425c
ddelfaus <ddelfaus@gmail.com>
2020-01-05 21:04:51 -0500

    put request for recipe done

--- recipes/recipe-model.js
+++ recipes/recipe-model.js
@@ -6,7 +6,9 @@ const db = require(`../database/dbConfig`)
 module.exports = {
 
     getRecipes,
-    addRecipe
+    addRecipe,
+    updateRecipe,
+    findId
    
 }
 
@@ -17,14 +19,25 @@ function getRecipes() {
     return db(`recipe`)
 }
 
-// function findId(id) {
-//     return db('recipe')
-//         .where({user_id: id})
-//         .first()
-// }
+function findId(id) {
+    return db('recipe')
+        .where({user_id: id})
+        .first()
+}
 
 function addRecipe(recipe) {
     return db
         .insert(recipe, 'id') 
         .into('recipe')
 }
+
+
+function updateRecipe(recipe) {
+    return db('recipe')
+        .where({id: recipe.id})
+        .update(recipe)
+        .then(() => {
+            return findId(recipe.id)
+        })
+}
+
--- recipes/recipe-router.js
+++ recipes/recipe-router.js
@@ -26,10 +26,22 @@ router.post('/', authorized, (req, res) => {
         res.status(201).json(newRecipe)
     })
     .catch (err => {
-        res.status(500).json({ message: "failed"})
+        res.status(500).json({ message: "failed, make sure you have all the needed fields"})
     })
 })
 
 
+router.put('/:id', authorized, (req, res) => {
+    const recipeData = { ...req.body, id: req.params.id}
+   
+    console.log(recipeData)
+    Recipes.updateRecipe(recipeData)
+        .then(edit => {
+            res.status(201).json(recipeData)
+        })
+        .catch (err => {
+            res.status(500).json({ message: "failed, make sure you have all the needed fields"})
+        })
+})
   
 module.exports = router;
